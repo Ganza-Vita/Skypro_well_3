db656977bae79e89d0b87ee7505518bd7077ba58
Bulat Galimov <bul.al@mail.ru>
2024-10-26 17:51:47 +0400

    modified "utils"

--- src/utils.py
+++ src/utils.py
@@ -4,13 +4,15 @@ import json
 
 def returns_a_list_of_transactions(file_path: str) -> list:
     """ Функция возвращает список словарей с транзакиями из JSON-файла """
-    with open(file_path, 'r', encoding='utf-8') as file_transaction:
-        return file_transaction.read()
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file_transaction:
+            return file_transaction.read()
+    except (FileNotFoundError, UnicodeDecodeError) as e:
+        return []
 
 
 
 
-
-file_path = 'C:\\Users\\Bulat\\PycharmProjects\\Skypro_well_3\\data\\operations.json'
+file_path = 'C:\\Users\\Bulat\\PycharmProjects\\Skypro_well_3\\data\\operatins.json'
 file_content = returns_a_list_of_transactions(file_path)
 print(file_content)
